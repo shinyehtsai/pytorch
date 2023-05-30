0563e317e1eab9fa16c89d457219a85c773dc2cd
Will Constable <whc@fb.com>
2023-05-30 16:48:10 +0000

    Add dynamo collections.deque support
    ghstack-source-id: 0a8b90ed841e382cbc59a817111ad7b6a19aeb17
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/102412

--- torch/_dynamo/variables/misc.py
+++ torch/_dynamo/variables/misc.py
@@ -702,6 +702,16 @@ class SkipFilesVariable(VariableTracker):
                 unimplemented(f"functools.wraps({fn})")
 
             return variables.LambdaVariable(wraps, **options)
+        elif self.value is collections.deque and not kwargs:
+            if len(args) == 0:
+                items = []
+            elif len(args) == 1:
+                items = args[0].items
+            else:
+                unimplemented("deque() with more than 1 arg not supported by dynamo")
+            return variables.lists.DequeVariable(
+                items, mutable_local=MutableLocal(), **options
+            )
         else:
             try:
                 path = inspect.getfile(self.value)
