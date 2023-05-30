35c7118f4e256f2caa93622b8fc2fed5946df12d
XiaobingSuper <xiaobing.zhang@intel.com>
2023-05-30 10:24:15 -0400

    inductor: improve the index range check for index_expr vec check
    ghstack-source-id: 9df49eddaae1eebe4c3a92c2686f50d8b492c778
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/102263

--- torch/_inductor/codegen/cpp.py
+++ torch/_inductor/codegen/cpp.py
@@ -18,9 +18,11 @@ import torch.fx
 from torch._inductor import dependencies
 from torch._inductor.ir import StorageBox, TensorBox
 from torch._prims_common import is_float_dtype
+from torch.utils._sympy.value_ranges import ValueRanges
 
 from .. import codecache, config, ir, metrics
 from ..codegen.wrapper import WrapperCodeGen
+from ..optimize_indexing import get_expr_range, range_expressable_in_32_bits
 from ..scheduler import SchedulerNode
 from ..utils import (
     cache_on_self,
@@ -29,6 +31,7 @@ from ..utils import (
     sympy_subs,
     sympy_symbol,
 )
+
 from ..virtualized import ops, V
 from .common import (
     BracesBuffer,
@@ -1984,8 +1987,6 @@ class CppVecKernelChecker(CppVecKernel):
 
             @staticmethod
             def index_expr(expr, dtype):
-                current_node: torch.fx.Node = V.interpreter.current_node
-
                 assert len(self.ranges) == len(self.itervars)
                 if not len(self.ranges) or not all(
                     not isinstance(range, sympy.Expr) or sympy.simplify(range).is_number
@@ -2010,28 +2011,23 @@ class CppVecKernelChecker(CppVecKernel):
 
                     opt_ctx: OptimizationContext = node_ctx.get_opt_ctx()
                     assert opt_ctx
-                    max_expr = expr.replace(
-                        ir.ModularIndexing, mod_indexing_rep
-                    ).replace(ir.FloorDiv, indexing_div_rep)
-                    min_expr = max_expr
-                    for idx in range(len(self.ranges)):
-                        max_expr = sympy.maximum(
-                            max_expr,
-                            self.itervars[idx],
-                            sympy.Interval(0, self.ranges[idx]),
+                    free_symbols = list(expr.free_symbols)
+                    vars_ranges = {
+                        k: ValueRanges(0, v)
+                        for k, v in zip(self.itervars, self.ranges)
+                        if k in free_symbols
+                    }
+                    expr_ranges = get_expr_range(expr, vars_ranges)
+                    if math.isinf(expr_ranges.lower) or math.isinf(expr_ranges.upper):
+                        can_use_int32 = False
+                    else:
+                        expr_ranges_int = ValueRanges(
+                            int(expr_ranges.lower), int(expr_ranges.upper)
                         )
-                        min_expr = sympy.minimum(
-                            min_expr,
-                            self.itervars[idx],
-                            sympy.Interval(0, self.ranges[idx]),
-                        )
-                    i32_iinfo = numpy.iinfo(numpy.int32)
+                        can_use_int32 = range_expressable_in_32_bits(expr_ranges_int)
                     if (
                         dtype == torch.int64
-                        and max_expr.is_number
-                        and min_expr.is_number
-                        and max_expr <= i32_iinfo.max
-                        and min_expr >= i32_iinfo.min
+                        and can_use_int32
                         and all(
                             user.target in VecCheckerProxy.bin_cmp_ops
                             for user in node_ctx.current_node.users
